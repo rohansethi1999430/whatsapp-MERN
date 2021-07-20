320242562ce11af1388e60a76997998bacc2f150
Rohan <rohansethi1999430@gmail.com>
2021-07-20 08:48:30 +0530

    Bug Fix: Name  not sent to pusher api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 
+import { useEffect } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
+import Pusher from 'pusher-js'
 
 function App() {
+
+  useEffect(() => {
+    
+  }, [])
+
+  useEffect(() => {
+    const pusher = new Pusher('257ecbbf7d3543e9da9d', {
+      cluster: 'ap2'
+    });
+
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', (data)=> {
+      alert(JSON.stringify(data));
+    });
+  }, [])
+  
+
+
   return (
     <div className="app">
       <div className="app__body">

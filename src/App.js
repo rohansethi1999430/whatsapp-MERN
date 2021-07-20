7af984ad95d5de93a08f1da4996fe71582bc6772
Rohan <rohansethi1999430@gmail.com>
2021-07-20 22:05:27 +0530

    Bug Fix: app crashing on starting and rendering data to frontend

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-
-import { useEffect } from 'react';
+import React  from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Chat from './Chat';
 import Sidebar from './Sidebar';
 import Pusher from 'pusher-js'
+ import axios from './axios'
 
 function App() {
-
+  const [messages,setMessages]=useState([])
   useEffect(() => {
-    
+    axios.get('/messages/sync')
+    .then(response=>{
+      
+      setMessages(response.data)
+    })
   }, [])
 
   useEffect(() => {
@@ -17,18 +22,23 @@ function App() {
     });
 
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', (data)=> {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', (newMessage)=> {
+      alert(JSON.stringify(newMessage));
+      setMessages([...messages,newMessage])
     });
-  }, [])
+    return()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
+  }, [messages])
   
-
+  console.log(messages)
 
   return (
     <div className="app">
       <div className="app__body">
       <Sidebar></Sidebar>
-      <Chat></Chat>
+      <Chat messages={messages}></Chat>
       </div>
     
   
